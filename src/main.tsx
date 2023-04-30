553a0c2a68535ee0c374fcb6fb689e13949134a1
itssimple <itssimple@users.noreply.github.com>
2023-04-30 07:23:15 +0000

    deploy: dd6dbf05bad0ca1dc561db34f8aaf11d84659d16

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "preact";
-import { log } from "./scripts/log";
-import { App } from "./app";
-import "./index.css";
-
-log("MAIN", "Starting app...");
-
-render(<App />, document.getElementById("app") as HTMLElement);
--- src/pages/main-page.tsx
+++
@@ -1,3 +0,0 @@
-export default function MainPage() {
-  return <>Main page - {import.meta.env.VITE_BUNGIE_API_AUTHURL}</>;
-}
