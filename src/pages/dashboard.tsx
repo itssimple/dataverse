aa4d8b36c1e313d0cec2fece8c66a9246c2c98ba
NoLifeKing <github-things@nolifeking85.tv>
2024-06-11 22:26:21 +0200

    feat: get logged out if you're not authed

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
+  const apiClient = window.apiClient;
+
+  apiClient.checkIfAuthenticated().then((result) => {
+    if (!result) {
+      location.href = "/";
+    }
+  });
+
   return <>Blep</>;
 }
