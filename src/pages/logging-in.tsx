82d226f3ed14f2e1cc8f967cecf42b0e3697553f
NoLifeKing <github-things@nolifeking85.tv>
2024-07-02 18:47:48 +0200

    feat: Handle API being down.

--- src/pages/logging-in.tsx
+++ src/pages/logging-in.tsx
@@ -21,55 +21,74 @@ export default function LoggingIn(props: D2AppState) {
   }
 
   // Check if the user has a valid token and/or is authenticated, if not, redirect to the main page so they can login
-  apiClient.checkIfAuthenticated().then(async (result) => {
-    if (!result) {
-      location.href = "/";
-      return;
-    }
+  apiClient
+    .checkIfAuthenticated()
+    .then(async (result) => {
+      if (!result) {
+        location.href = "/";
+        return;
+      }
 
-    log("LOGIN", "Authenticated, checking manifests");
+      log("LOGIN", "Authenticated, checking manifests");
 
-    setLoadingText("Checking manifest ...");
-    let manifestVersion = await apiClient.checkManifestVersion();
+      setLoadingText("Checking manifest ...");
+      let manifestVersion = await apiClient.checkManifestVersion();
 
-    if (manifestVersion === null || manifestVersion === undefined) {
-      setLoadingText(
-        "Something is wrong with Destiny 2 (or this app), please reload the page."
-      );
-      return;
-    }
+      if (manifestVersion === null || manifestVersion === undefined) {
+        setLoadingText(
+          "Something is wrong with Destiny 2 (or this app), please reload the page."
+        );
+        return;
+      }
 
-    log("LOGIN", manifestVersion);
+      log("LOGIN", manifestVersion);
 
-    setLoadingText("Loading profile data");
+      setLoadingText("Loading profile data");
 
-    await apiClient.getLastPlayedCharacter();
+      await apiClient.getLastPlayedCharacter();
 
-    setLoadingText("Checking for missing definitions");
+      setLoadingText("Checking for missing definitions");
 
-    let missingDefinitions = await apiClient.checkStoredDefinitions(false);
+      let missingDefinitions = await apiClient.checkStoredDefinitions(false);
 
-    if (missingDefinitions.length > 0) {
-      setLoadingText(
-        `Downloading ${missingDefinitions.length} missing definition(s)`
-      );
-      await apiClient.checkStoredDefinitions(true);
-    }
+      if (missingDefinitions.length > 0) {
+        setLoadingText(
+          `Downloading ${missingDefinitions.length} missing definition(s)`
+        );
+        await apiClient.checkStoredDefinitions(true);
+      }
 
-    setLoadingText("Loading data...");
-    await apiClient.loadDataFromStorage();
+      setLoadingText("Loading data...");
+      await apiClient.loadDataFromStorage();
 
-    props.isDataLoaded.value = true;
-
-    setTimeout(() => {
-      setLoadingText("Opening application...");
-      eventEmitter.emit("manifests-loaded");
+      props.isDataLoaded.value = true;
 
       setTimeout(() => {
-        location.href = "/#/dashboard";
+        setLoadingText("Opening application...");
+        eventEmitter.emit("manifests-loaded");
+
+        setTimeout(() => {
+          location.href = "/#/dashboard";
+        }, 1000);
       }, 1000);
-    }, 1000);
-  });
+    })
+    .catch(async (error) => {
+      if (error.status === 503) {
+        // Bungie API is down
+        setLoadingText("Bungie API is down, retrying in a minute.");
+        setTimeout(() => {
+          location.href = "/";
+        }, 60000);
+      } else {
+        setLoadingText(
+          "An error occurred while trying to log in, please try again."
+        );
+
+        console.error(error);
+        console.error(await error.json());
+      }
+      //location.href = "/";
+    });
 
   return (
     <>
